23c42b2504abf2d86717531914a907d6d8b7b238
yonatan190799 <88882175+yonatan190799@users.noreply.github.com>
2021-12-07 14:33:26 +0800

    Add files via upload

---
+++ Additional_Assignment/src/App.js
@@ -0,0 +1,278 @@
+
+import './App.css';
+import { Col,Container,Row,Card,Button,InputGroup,FormControl, NavDropdown } from 'react-bootstrap';
+
+function App() {
+  return (
+    
+    <div className="App">
+
+    {/* Navbar */}
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="navbar-collapse collapse  order-1 order-md-0 dual-collapse2 ">
+          <ul class="navbar-nav mx-auto">
+            <NavDropdown title="ALL TYPES" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Chair</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Table</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">wardrobe</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
+          </ul>
+        </div>
+        <div class="navbar-collapse collapse  order-2 order-md-1 dual-collapse2">
+          <ul class="navbar-nav mx-auto  ">
+              <li class="nav-item active">
+                  <a class="nav-link" href="#">Popular Products</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">Low Price</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">High Price</a>
+              </li>
+          </ul>
+        </div>
+        <div class="navbar-collapse collapse w-150 order-3 order-md-2 dual-collapse2">
+          <ul class="navbar-nav mx-auto">
+            <NavDropdown title="PRICE" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Rp. 0 - 1.000.000</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Rp. 1.000.000 - 5.000.000- </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Rp. 5.000.000 - .........</NavDropdown.Item>
+            </NavDropdown>
+          </ul>
+        </div>
+      </nav>
+
+      <Container>
+
+        {/* Card 1 */}
+        <Row>
+          <Col>
+          <Card style={{ width: '400px' ,top:'20px'}}>
+            <Row>
+            <div className="mt-4 mb-4">
+              <Button variant="light" size="sl" style={{ marginRight: '200px'}}>
+                New
+              </Button>{' '}
+              <Button variant="light" size="sl">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list-ul" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+              </svg>
+              </Button>
+              <Button variant="light" size="sl">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+              </svg>
+              </Button>
+            </div>
+            </Row>
+            
+            {/* Image  */}
+            <Row>
+            <Card.Img variant="top" src="kursi.jpeg" />
+            </Row>
+            {/* Title */}
+            <Row>
+            <Card.Body>
+                <Card.Title>Single Chair</Card.Title>
+              </Card.Body>
+            </Row> 
+            {/* Info */}
+            <Row className="mt-4 mb-4">
+            <Col >
+            {/* rattings*/}
+              <div className="mt-2" style={{marginLeft:'12px' }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="silver" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+              </div>
+
+              {/* Price and cart button */}
+            </Col>
+                <Col variant="dark">
+                  <InputGroup  className="mb-2" style={{ width: '250px' , marginRight:'10px',}}>
+                      <FormControl
+                      placeholder="Rp. 1.200.000"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                      style={{ width: '50px'}}
+                      disabled
+                    />
+                      <InputGroup.Text>
+                      <Button variant="light"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                      </svg></Button> {' '}
+                      </InputGroup.Text>
+                  </InputGroup>
+                </Col>
+            </Row>
+          </Card>
+          </Col>
+
+
+         {/* Card 2*/}
+         <Col>
+          <Card style={{ width: '400px' ,top:'20px'}}>
+            <Row>
+            <div className="mt-4 mb-4">
+              <Button variant="light" size="sl" style={{ marginRight: '200px'}}>
+                New
+              </Button>{' '}
+              <Button variant="light" size="sl">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list-ul" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+              </svg>
+              </Button>
+              <Button variant="light" size="sl">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+              </svg>
+              </Button>
+            </div>
+            </Row>
+            
+            {/* Image  */}
+            <Row>
+            <Card.Img variant="top" src="meja.jpeg" />
+            </Row>
+            {/* Title */}
+            <Row>
+            <Card.Body>
+                <Card.Title>Single Table</Card.Title>
+              </Card.Body>
+            </Row> 
+            {/* Info */}
+            <Row className="mt-4 mb-4">
+            <Col >
+              <div className="mt-2" style={{marginLeft:'12px' }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="silver" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+              </div> 
+            </Col>
+                <Col variant="dark">
+                <InputGroup  className="mb-2" style={{ width: '250px' , marginRight:'10px',}}>
+                    <FormControl
+                    placeholder="Rp. 500.000"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    style={{ width: '50px'}}
+                    disabled
+                  />
+                <InputGroup.Text>
+                <Button variant="light"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </svg></Button> {' '}
+                </InputGroup.Text>
+              </InputGroup>
+                </Col>   
+            </Row>
+          </Card>
+         </Col>
+
+
+          {/* Card 3 */}
+          <Col>
+          <Card style={{ width: '400px' ,top:'20px'}}>
+            <Row>
+            <div className="mt-4 mb-4">
+              <Button variant="light" size="sl" style={{ marginRight: '200px'}}>
+                New
+              </Button>{' '}
+              <Button variant="light" size="sl">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list-ul" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+              </svg>
+              </Button>
+              <Button variant="light" size="sl">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+              </svg>
+              </Button>
+            </div>
+            </Row>
+            
+            {/* Image  */}
+            <Row>
+            <Card.Img variant="top" src="wardrobe.jpeg" />
+            </Row>
+            {/* Title */}
+            <Row>
+            <Card.Body>
+                <Card.Title>Modern Wardrobe</Card.Title>
+              </Card.Body>
+            </Row> 
+            {/* Info */}
+            <Row className="mt-4 mb-4">
+            <Col >
+                  <div className="mt-2" style={{marginLeft:'12px' }}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="gold" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="silver" class="bi bi-star-fill" viewBox="0 0 16 16">
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> 
+                </svg>
+              </div>
+            </Col>
+            <Col variant="dark">
+                <InputGroup  className="mb-2" style={{ width: '250px' , marginRight:'10px'}}>
+                    <FormControl
+                    placeholder="Rp. 10.000.000"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    style={{ width: '50px'}}
+                    disabled
+                  />
+                <InputGroup.Text>
+                <Button variant="light"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </svg></Button> {' '}
+                </InputGroup.Text>
+              </InputGroup>
+            </Col>   
+            </Row>
+          </Card>
+          </Col>        
+        </Row>
+
+      </Container>
+
+    </div>
+
+  );
+}
+
+export default App;
